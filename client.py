33b46cc2a83967a4a4be74255e1429d2796266a7
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-08 11:46:07 +0530

    Added a moving block using pygame

---
+++ client.py
@@ -0,0 +1,68 @@
+import pygame
+
+width = 500
+height = 500
+
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+
+
+class Player:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x, y, width, height)
+        self.vel = 0.5
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def redrawWindow(player, win):
+
+    win.fill((0, 0, 0))
+    player.draw(win)
+    pygame.display.update()
+
+
+def main():
+
+    run = True
+
+    p = Player(50, 50, 100, 100, (0, 255, 0))
+
+    while run:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                pygame.quit()
+                run = False
+
+        p.move()
+        redrawWindow(p, win)
+
+
+if __name__ == '__main__':
+    main()
