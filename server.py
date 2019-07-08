dd8e4635901d72a09ba47e4d40f3d642d00d97d6
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-08 17:02:07 +0530

    Added network script

--- server.py
+++ server.py
@@ -18,12 +18,15 @@ except socket.error as e:
 s.listen(2)
 print("Server started. Waiting for connection")
 
+
 def threaded_client(conn):
     """
     Recieve data from client
     :param conn: connection
     :return: None
     """
+
+    conn.send(str.encode("Connected"))
     reply = ""
     while True:
         try:
@@ -41,10 +44,12 @@ def threaded_client(conn):
 
         except:
             break
+    print("Lost connection")
+    conn.close()
 
 
 while True:
     conn, addr = s.accept()    # Connect to a new client
-    print("Connected to: " + addr)
+    print("Connected to: ", addr)
 
     _thread.start_new_thread(threaded_client, (conn,))    # Start new thread for the client
