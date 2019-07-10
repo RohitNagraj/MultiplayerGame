7edeb12d8cd74e9ef23c1c4f00062fc83f3c1da6
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-10 12:12:03 +0530

    Modified server to send and recieve positions

--- server.py
+++ server.py
@@ -18,20 +18,22 @@ except socket.error as e:
 s.listen(2)
 print("Server started. Waiting for connection")
 
+pos = [(0, 0), (100, 100)]
 
-def threaded_client(conn):
+
+def threaded_client(conn, player):
     """
     Recieve data from client
+    :param player: the current player we are dealing with
     :param conn: connection
     :return: None
     """
 
-    conn.send(str.encode("Connected"))
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048))
 
             if not data:
                 print('Disconnected')
@@ -48,8 +50,31 @@ def threaded_client(conn):
     conn.close()
 
 
+def read_pos(pos):
+    """
+    Converts string data coming from server to tuple
+    :param pos: string data
+    :return: position tuple
+    """
+    pos = pos.strip(',')
+    return int(pos[0]), int(pos[1])
+
+
+def make_pos(tup):
+    """
+    Converts tuple data from client to sendable server string
+    :param tup: tuple data
+    :return: string data
+    """
+    return str(tup[0] + ',' + tup[1])
+
+
+current_player = 0
+
 while True:
     conn, addr = s.accept()    # Connect to a new client
     print("Connected to: ", addr)
 
-    _thread.start_new_thread(threaded_client, (conn,))    # Start new thread for the client
+    _thread.start_new_thread(threaded_client, (conn, current_player))    # Start new thread for the client
+
+    current_player += 1
