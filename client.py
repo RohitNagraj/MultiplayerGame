ceb86d30442c1d4320628e28124ad5c757d49829
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-10 12:01:30 +0530

    Added networking to client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import pygame
+from network import Network
 
 # Dimensions of the screen
 width = 500
@@ -11,6 +12,9 @@ pygame.display.set_caption("Client")  # Title of the window
 
 # To hold no. of clients created
 clientNumber = 0
+
+# To count the no. of frames
+clock = pygame.time.Clock()
 
 
 class Player:
@@ -62,35 +66,81 @@ class Player:
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(self):
+        """
+        Defines the tuple required to create a rectangle
+        :return: tuple
+        """
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redraw_window(player, win):
+def redraw_window(player1, player2, win):
     """
     Draws the player window again
-    :param player: Player class object
+    :param player1: Player1 class object
+    :param player2: Player2 class object
     :param win: Window to draw player on
     :return: None
     """
     win.fill((0, 0, 0))        # fill with black bg color
-    player.draw(win)           # Draw the player rectangle on the window
+    player1.draw(win)           # Draw the player rectangle on the window
+    player2.draw(win)
     pygame.display.update()    # update the window to display the changes
+
+
+def read_pos(pos):
+    """
+    Converts string data coming from server to tuple
+    :param pos: string data
+    :return: position tuple
+    """
+    pos = pos.strip(',')
+    return int(pos[0]), int(pos[1])
+
+
+def make_pos(tup):
+    """
+    Converts tuple data from client to sendable server string
+    :param tup: tuple data
+    :return: string data
+    """
+    return str(tup[0] + ',' + tup[1])
 
 
 def main():
 
     run = True
 
-    p = Player(50, 50, 100, 100, (0, 255, 0))
+    # Create a new client
+    n = Network()
+    start_pos = read_pos(n.get_pos())    # Gets the starting position of that client from server
+
+    p1 = Player(start_pos[0], start_pos[1], 100, 100, (0, 255, 0))    # Define the player at that starting position
+    p2 = Player(0, 0, 100, 100, (0, 255, 0))    # Defines player 2 at its position
 
     while run:
+
+        clock.tick(60)
+
+        # Send position of p1. It returns position of p2.
+        p2pos = read_pos(n.send((p1.x, p1.y)))
+        # Update the position of p2 accordingly
+        p2.x = p2pos[0]
+        p2.y = p2pos[1]
+        p2.update()
+
+        # To check if the game has to be quit
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 pygame.quit()
                 run = False
 
-        p.move()
-        redraw_window(p, win)
+        # Check for move from p1
+        p1.move()
+        # Redraw the window with whatever updated positions of p1 and p2 are
+        redraw_window(p1, p2, win)
 
 
 if __name__ == '__main__':
--- network.py
+++ network.py
@@ -3,20 +3,42 @@ import socket
 
 class Network:
 
+    """
+    Connects a new client to the server and provides its network actions
+    """
+
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.0.102"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+
+    def get_pos(self):
+        """
+        :return: the position of the client player
+        """
+        return self.pos
 
     def connect(self):
+        """
+        Connects the client to the server
+        :return: the data received from the server
+        """
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
 
-
-n = Network()
+    def send(self, data):
+        """
+        Sends data from client to server and receives data from server
+        :param data: data to send to server
+        :return: data received from server
+        """
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
